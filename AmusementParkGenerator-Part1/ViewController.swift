5231445dc9f0434e88808de28e7c7069795950e1
Marcél Busschers <marcelbusschers@gmail.com>
2018-12-17 16:27:28 +0100

    Initial Commit

---
+++ AmusementParkGenerator-Part1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AmusementParkGenerator-Part1
+//
+//  Created by Marcel Busschers on 2018/12/17.
+//  Copyright © 2018 Marcel Busschers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
